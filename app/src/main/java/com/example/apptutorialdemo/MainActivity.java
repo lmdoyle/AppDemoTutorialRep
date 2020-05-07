87e9051a203b1376966af97218dedbfa341beab5
lmdoyle <lmdoyle@mcdonogh.org>
2020-05-06 21:57:44 -0400

    Made changes

--- app/src/main/java/com/example/apptutorialdemo/MainActivity.java
+++ app/src/main/java/com/example/apptutorialdemo/MainActivity.java
@@ -3,8 +3,20 @@ package com.example.apptutorialdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+
+        Log.i("Info","Button Pressed");
+
+    }
+
+    public void clickFunction2(View view){
+        Log.i("Info2", "Info Button has been pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
